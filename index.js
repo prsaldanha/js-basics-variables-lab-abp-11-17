759506ba3e448e2dc99656570efb7aa0012b39f7
prem_saldanha@yahoo.ca <prem_saldanha@yahoo.ca>
2017-11-11 18:33:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const companyName = 'Scuber';// Code your solution in this file!
 companyName = 'specialCompany';
 
 let mostProfitableNeighborhood = 'Chelsea';
-const mostProfitableNeighborhood = 'Upper West Side';
+let  mostProfitableNeighborhood = 'Upper West Side';
 let companyCeo = 'Susan Smith';
-const companyCeo = 'Lauren Hart';
+companyCeo = 'Lauren Hart';
